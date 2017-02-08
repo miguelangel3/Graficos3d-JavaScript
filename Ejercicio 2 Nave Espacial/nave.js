a9440d7a627c3695aa8f878a78019d4313d3d9d5
miguelangel3 <miguelangel.abb@gmail.com>
2017-02-08 18:11:13 +0100

    Añado movimiento

--- Ejercicio 2 Nave Espacial/nave.js
+++ Ejercicio 2 Nave Espacial/nave.js
@@ -3,58 +3,60 @@
  var ctx;
 const desp = 3;
 
-function triangle(id,x,y,color) {
+function Triangle(id,x,y,color) {
 	this.id = id;
 	this.x = x;
 	this.y = y;
 	this.color = color;
+  that=this;
 		console.log("estoy entrando en la función");
 	this.draw =function(){
 		console.log("Estoy pintando");
-  
     // Triángulo rellenado
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.restore();
     ctx.beginPath();
-    ctx.moveTo(x,x);
+    ctx.moveTo(that.x,that.x);
     console.log(x);
-    ctx.lineTo(x,y);
-    ctx.lineTo(y,x);
-    ctx.fillStyle = color;
+    ctx.lineTo(that.x,that.y);
+    ctx.lineTo(that.y,that.x);
+    ctx.fillStyle = that.color;
     ctx.fill();
-
    //this.move = function(val)
 
-	}()
-	this.move = function(despx){
-		x= x +despx;
+	}
+  this.angulo = function(){
+    ctx.rotate()
+  }
+	this.move = function(orientacion){
+    if (orientacion == "derecha"){
+      that.x = that.x +10;
+      that.y = that.y +10
+    }else{
+      that.x = that.x -10;
+      that.y = that.y -10;
+    }
 	}
 }
 
-function getShape(id) {
-  for(x in shapes) {
-    if(shapes[x].id === id)
-      return shapes[x];
-  }
-}
 
 function keyHandler(event){
-	var obj;
-	obj = getShape("c1");
-	//if(obj === undefined)
-	//return;
+  console.log("EStyo dentro de KeyHAndler");
 
 	switch(event.key) {
 		case "ArrowLeft":
 			console.log("izquierda");
-			obj.move(-desp);
-			obj.draw();
+			t1.move("izquierda");
+			t1.draw();
 		break;
 		case "ArrowRight":
-			obj.move(+desp);
+      console.log("derecha");
+			t1.move("derecha");
+      t1.draw();
 		break;
 	default:
 	console.log("Key not handled");
 	}
-
 }
 
 function main(){
@@ -64,7 +66,11 @@ function main(){
 		return false;
   }
   	ctx = canvas.getContext('2d');
-  	document.addEventListener('keydown', keyHandler, false);
-
-  	shapes.push(new triangle("t1", 80, 25,"#FFF000"));
+  	t1= new Triangle("t1", 50, 25,"#FFF000");
+    t1.draw();
+    document.addEventListener('keydown', keyHandler, false);
 }
+
+//1 mover nave espacial
+//2 torpedos
+//3 Asteroides
