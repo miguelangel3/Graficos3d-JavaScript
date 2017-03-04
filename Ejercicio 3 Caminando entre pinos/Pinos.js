e761bec70fb8f25d49fed287b1c672951153a990
miguelangel3 <miguelangel.abb@gmail.com>
2017-03-04 16:58:13 +0100

    Pinto los pinos en todo el plano XZ y les doyuna altura y aleatoria

--- Ejercicio 3 Caminando entre pinos/Pinos.js
+++ Ejercicio 3 Caminando entre pinos/Pinos.js
@@ -9,9 +9,10 @@ var VSHADER_SOURCE =
   'attribute vec4 a_Position;\n' +
   'attribute vec4 a_Color;\n' +
   'uniform mat4 u_MvpMatrix;\n' +
+   'uniform mat4 u_xformMatrix;\n' +
   'varying vec4 v_Color;\n' +
   'void main() {\n' +
-  '  gl_Position = u_MvpMatrix * a_Position;\n' +
+  '  gl_Position = u_MvpMatrix *u_xformMatrix * a_Position;\n' +
   '  v_Color = a_Color;\n' +
   '}\n';
 
@@ -25,13 +26,42 @@ var FSHADER_SOURCE =
   '  gl_FragColor = v_Color;\n' +
   '}\n';
 
+
+function alturaPino(modelMatrix,projMatrix,viewMatrix,mvpMatrix){
+   
+
+   var Sy =  Math.floor(Math.random()*5)
+   var Sx = Sy/2
+   var Sz = Sx;
+   var xformMatrix = new Float32Array([
+      Sx,   0.0,  0.0,  0.0,
+      0.0,  Sy,   0.0,  0.0,
+      0.0,  0.0,  Sz,   0.0,
+      0.0,  0.0,  0.0,  1.0
+   ]);
+   // Pass the rotation matrix to the vertex shader
+   var u_xformMatrix = gl.getUniformLocation(gl.program, 'u_xformMatrix');
+   if (!u_xformMatrix) {
+      console.log('Failed to get the storage location of u_xformMatrix');
+      return;
+   }
+     //modelMatrix.setScale(Sx,Sy,Sz);
+    mvpMatrix.set(projMatrix).multiply(viewMatrix).multiply(modelMatrix);
+
+   gl.uniformMatrix4fv(u_xformMatrix, false, xformMatrix);
+
+}
+
+
+
+
 function plantarPino(modelMatrix,projMatrix,viewMatrix,mvpMatrix,n){
 
-  for (var i = 0; i < 25; i++) {
+  for (var i = 0; i < 50; i++) {
     var positionx;
     var positiony;
-    positionx = Math.floor(Math.random()*5);
-    positionz = Math.floor(Math.random()*5);
+    positionx = Math.floor(Math.random()*10)-5;
+    positionz = Math.floor(Math.random()*10)-5;
 
     gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
     
@@ -40,79 +70,29 @@ function plantarPino(modelMatrix,projMatrix,viewMatrix,mvpMatrix,n){
     mvpMatrix.set(projMatrix).multiply(viewMatrix).multiply(modelMatrix);
     // Pass the model view projection matrix to u_MvpMatrix
     gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
+    
+    alturaPino(modelMatrix,projMatrix,viewMatrix,mvpMatrix);
 
     gl.drawArrays(gl.TRIANGLES, 0, n);   // Draw the triangles
   }
 
 }
 
-function main() {
-  // Retrieve <canvas> element
-  var canvas = document.getElementById('webgl');
-
-  // Get the rendering context for WebGL
-  gl = getWebGLContext(canvas);
-  if (!gl) {
-    console.log('Failed to get the rendering context for WebGL');
-    return;
+function keydown(ev, gl, n, u_ProjMatrix, projMatrix, nf) {
+   switch(ev.keyCode){
+      case 39: g_near += 0.01; 
+         break;  
+      case 37: g_near -= 0.01; 
+         break;  
+      case 38: g_far += 0.01;  
+         break;  
+      case 40: g_far -= 0.01;  
+         break;  
+      default: return; 
   }
-
-  // Initialize shaders
-  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-    console.log('Failed to intialize shaders.');
-    return;
-  }
-
-  // Set the vertex coordinates and color (the blue triangle is in the front)
-  var n = initVertexBuffers(gl);
-  if (n < 0) {
-    console.log('Failed to set the vertex information');
-    return;
-  }
-
-  // Specify the color for clearing <canvas>
-  gl.clearColor(0, 0, 0, 1);
-  gl.enable(gl.DEPTH_TEST);
-
-  // Get the storage location of u_MvpMatrix
-  u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
-  if (!u_MvpMatrix) { 
-    console.log('Failed to get the storage location of u_MvpMatrix');
-    return;
-  }
-
-  var modelMatrix = new Matrix4(); // Model matrix
-  var viewMatrix = new Matrix4();  // View matrix
-  var projMatrix = new Matrix4();  // Projection matrix
-  var mvpMatrix = new Matrix4();   // Model view projection matrix
-
-  // Calculate the model, view and projection matrices
-  modelMatrix.setTranslate(0.75, 0, 0);
-
-  viewMatrix.setLookAt(0.8, 1.70, 8.0, 0.0, 0.0, 0.0, 0, 1.0,0.0);
-  projMatrix.setPerspective(40, canvas.width/canvas.height, 1, 100);
-
-  // Calculate the model view projection matrix
-  mvpMatrix.set(projMatrix).multiply(viewMatrix).multiply(modelMatrix);
-
-  // Pass the model view projection matrix to u_MvpMatrix
-  gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
-
-  gl.clear(gl.COLOR_BUFFER_BIT);   // Clear <canvas>
-
-  gl.drawArrays(gl.TRIANGLES, 0, n);   // Draw the triangles
-  
-  plantarPino(modelMatrix,projMatrix,viewMatrix,mvpMatrix,n);
-
-　/*// Prepare the model matrix for another pair of triangles
- // modelMatrix.setTranslate(-0.75, 0, 0); //-->con esto pinto ostros dos triángulos.
-  // Calculate the model view projection matrix
-  mvpMatrix.set(projMatrix).multiply(viewMatrix).multiply(modelMatrix);
-  // Pass the model view projection matrix to u_MvpMatrix
-  gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
-
-  gl.drawArrays(gl.TRIANGLES, 0, n);   // Draw the triangles*/
 }
+
+
 
 function initVertexBuffers(gl) {
 var verticesColors = new Float32Array([
@@ -164,3 +144,78 @@ var verticesColors = new Float32Array([
   return n;
 }
 
+
+function main() {
+  // Retrieve <canvas> element
+  var canvas = document.getElementById('webgl');
+
+  // Get the rendering context for WebGL
+  gl = getWebGLContext(canvas);
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+    return;
+  }
+
+  // Initialize shaders
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log('Failed to intialize shaders.');
+    return;
+  }
+
+  // Set the vertex coordinates and color (the blue triangle is in the front)
+  var n = initVertexBuffers(gl);
+  if (n < 0) {
+    console.log('Failed to set the vertex information');
+    return;
+  }
+
+  // Specify the color for clearing <canvas>
+  gl.clearColor(0, 0, 0, 1);
+  gl.enable(gl.DEPTH_TEST);
+
+  // Get the storage location of u_MvpMatrix
+  u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
+  if (!u_MvpMatrix) { 
+    console.log('Failed to get the storage location of u_MvpMatrix');
+    return;
+  }
+
+  var modelMatrix = new Matrix4(); // Model matrix
+  var viewMatrix = new Matrix4();  // View matrix
+  var projMatrix = new Matrix4();  // Projection matrix
+  var mvpMatrix = new Matrix4();   // Model view projection matrix
+
+  // Calculate the model, view and projection matrices
+  modelMatrix.setTranslate(0, 0, 0);
+
+  viewMatrix.setLookAt(0.8, 1.70, 8.0, 0.0, 0.0, 0.0, 0, 1.0,0.0);
+  projMatrix.setPerspective(60, canvas.width/canvas.height, 1, 100);
+
+  // Calculate the model view projection matrix
+  mvpMatrix.set(projMatrix).multiply(viewMatrix).multiply(modelMatrix);
+
+  // Pass the model view projection matrix to u_MvpMatrix
+  gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
+
+  gl.clear(gl.COLOR_BUFFER_BIT);   // Clear <canvas>
+
+  gl.drawArrays(gl.TRIANGLES, 0, n);   // Draw the triangles
+  
+  plantarPino(modelMatrix,projMatrix,viewMatrix,mvpMatrix,n);
+
+  document.onkeydown = function(ev){ 
+   keydown(ev, gl, n, u_ProjMatrix, projMatrix, nf);
+
+   }
+
+
+
+　/*// Prepare the model matrix for another pair of triangles
+ // modelMatrix.setTranslate(-0.75, 0, 0); //-->con esto pinto ostros dos triángulos.
+  // Calculate the model view projection matrix
+  mvpMatrix.set(projMatrix).multiply(viewMatrix).multiply(modelMatrix);
+  // Pass the model view projection matrix to u_MvpMatrix
+  gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
+
+  gl.drawArrays(gl.TRIANGLES, 0, n);   // Draw the triangles*/
+}
