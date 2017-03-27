671be6be5deb4d35c739586d1a432f877e591347
miguelangel3 <miguelangel.abb@gmail.com>
2017-03-27 13:03:06 +0200

    Consigo pintar el suelo y los pinos con texturasgit add --all!

--- Ejercicio 4 Caminando entre pinos con textura/caminando_entre_pinos_textura.js
+++ Ejercicio 4 Caminando entre pinos con textura/caminando_entre_pinos_textura.js
@@ -4,36 +4,17 @@ var canvas;
 var gl;
 var u_MvpMatrix;
 
+var myScene = [];
+var myBuffers = [];
+
 var modelMatrix = []; // Model matrix
 var viewMatrix = new Matrix4();  // View matrix
 var projMatrix = new Matrix4();  // Projection matrix
 var mvpMatrix = new Matrix4();   // Model view projection matrix
 
-//shader sin textura 
-/*
+ //Shader con textura
+
 var VSHADER_SOURCE =
-  'attribute vec4 a_Position;\n' +
-  'attribute vec4 a_Color;\n' +
-  'uniform mat4 u_MvpMatrix;\n' +
-  'varying vec4 v_Color;\n' +
-  'void main() {\n' +
-  '  gl_Position = u_MvpMatrix*a_Position;\n' +
-  '  v_Color = a_Color;\n' +
-  '}\n';
-
-// Fragment shader program
-var FSHADER_SOURCE =
-  '#ifdef GL_ES\n' +
-  'precision mediump float;\n' +
-  '#endif\n' +
-  'varying vec4 v_Color;\n' +
-  'void main() {\n' +
-  '  gl_FragColor = v_Color;\n' +
-  '}\n';*/
-
-  //Shader con textura
-
-  var VSHADER_SOURCE =
   'attribute vec3 a_VertexPosition;\n' +
   'attribute vec2 a_TextureCoord;\n' +
   'uniform mat4 u_MvpMatrix;\n' +
@@ -56,8 +37,37 @@ var FSHADER_SOURCE =
 
 
 
+
+
+function pinoVarBuffer(Texture,VerticesBuffer,VerticesTextureCoordBuffer,
+               VerticesIndicesBuffer) {
+
+   this.id = "PB1"
+   this.Texture = Texture;
+   this.VerticesBuffer = VerticesBuffer;
+   this.VerticesTextureCoordBuffer = VerticesTextureCoordBuffer;
+   this.VerticesIndicesBuffer = VerticesIndicesBuffer;
+
+}
+
+function floorVarBuffer(Texture,VerticesBuffer,VerticesTextureCoordBuffer,
+               VerticesIndicesBuffer){
+
+   this.id = "FB1";
+   this.Texture = Texture;
+   this.VerticesBuffer = VerticesBuffer;
+   this.VerticesTextureCoordBuffer = VerticesTextureCoordBuffer;
+   this.VerticesIndicesBuffer = VerticesIndicesBuffer;
+   this.getid = function(){
+
+      return this.id;
+   }
+
+}
+
 function camara (pasos,angle,pasosx,pasosy,speed,moveAngle,alturaOjos,anglez){
 
+   this.id = "C1"
    this.pasos = pasos;
    this.angle = angle;
    this.pasosx = pasosx;
@@ -69,24 +79,27 @@ function camara (pasos,angle,pasosx,pasosy,speed,moveAngle,alturaOjos,anglez){
 
  }
 
-function pinoVarBuffer(pinoTexture,pinoVerticesBuffer,pinoVerticesTextureCoordBuffer,
-               pinoVerticesIndicesBuffer) {
 
-   this.pinoTexture = pinoTexture;
-   this.pinoVerticesBuffer = pinoVerticesBuffer;
-   this.pinoVerticesTextureCoordBuffer = pinoVerticesTextureCoordBuffer;
-   this.pinoVerticesIndicesBuffer = pinoVerticesIndicesBuffer;
+function Floor(mMatrix){
 
-
+   this.id = "F1"
+   this.mMatrix = mMatrix;
 }
 
-function Pino(id,x,y,z,matrix){
+function Pino(x,y,z,mMatrix){
 
-   this.id = id;
+   this.id = "P";
    this.x = x;
    this.y = y;
    this.z = z;
-   this.matrix = matrix;
+   this.mMatrix = mMatrix;
+}
+
+function getShape(array,id) {
+  for(x in array) {
+      if(array[x].id === id)
+      return array[x];
+  }
 }
 
 function drawScene(){
@@ -94,19 +107,23 @@ function drawScene(){
    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
    requestAnimationFrame(drawScene);//,projMatrix,viewMatrix,mvpMatrix);
 
-   for (x in modelMatrix){
-     /* var n = 6; // initVertexBuffers(gl); --> la verdad es que no se para que tenía esta llamada en teoría n es el número de vertices
-      if (n < 0) {
-         console.log('Failed to set the vertex information');
-         return;
-      }*/
+   var y; //Esta variable la declaro para selccionar uno de los dos buffers para pintar correctamente.
 
+   for (x in myScene){
+    
       viewMatrix.setLookAt(camara1.pasosx,camara1.pasosy,camara1.alturaOjos,camara1.pasosx+Math.cos(camara1.angle),
                   camara1.pasosy+Math.sin(camara1.angle),camara1.alturaOjos+ 0.01*Math.sin(camara1.anglez), 0,0,1);
 
-      mvpMatrix.set(projMatrix).multiply(viewMatrix).multiply(modelMatrix[x].matrix);
 
       //lamada a los buffers para pintar con texturas 
+      if (myScene[x].id === "F1"){
+         console.log("encontrado F1")
+         y = 0;
+      }else{
+         y = 1;
+      }
+
+      mvpMatrix.set(projMatrix).multiply(viewMatrix).multiply(myScene[x].mMatrix);
 
       gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
 
@@ -116,27 +133,27 @@ function drawScene(){
       var textureCoordAttribute = gl.getAttribLocation(gl.program, "a_TextureCoord");
       gl.enableVertexAttribArray(textureCoordAttribute);
 
-      gl.bindBuffer(gl.ARRAY_BUFFER,pinoVarBuffer1.pinoVerticesBuffer);
+      gl.bindBuffer(gl.ARRAY_BUFFER,myBuffers[y].VerticesBuffer);
       gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
 
-      gl.bindBuffer(gl.ARRAY_BUFFER,pinoVarBuffer1.pinoVerticesTextureCoordBuffer);
+      gl.bindBuffer(gl.ARRAY_BUFFER,myBuffers[y].VerticesTextureCoordBuffer);
       gl.vertexAttribPointer(textureCoordAttribute, 2, gl.FLOAT, false, 0, 0);
 
       //console.log(objfloorvar1.floorTexture);
       gl.activeTexture(gl.TEXTURE0);
-      gl.bindTexture(gl.TEXTURE_2D,pinoVarBuffer1.pinoTexture);
+      gl.bindTexture(gl.TEXTURE_2D,myBuffers[y].Texture);
 
       gl.uniform1i(gl.getUniformLocation(gl.program, "u_Sampler"), 0);
 
    
-      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER,pinoVarBuffer1.pinoVerticesIndicesBuffer);
+      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER,myBuffers[y].VerticesIndicesBuffer);
       gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
 
-      var n = 0;
+      //var n = 0;
 
-      n = n + 1;
+      //n = n + 1;
 
-      console.log("pinta: " + n);
+      //console.log("pinta: " + n);
       //llamada a los buffers para pintar sin texturas 
 
       //gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
@@ -145,9 +162,9 @@ function drawScene(){
    }
 }
 
-function plantarPino(/*n,*/NumPinos,bosquex,bosquey){
+function plantarPino(NumPinos,bosquex,bosquey){
 
-  for (var i = 0; i < NumPinos; i++) {
+  for (var i = 1; i < NumPinos; i++) {
 
       var positionx;
       var positiony;
@@ -163,13 +180,13 @@ function plantarPino(/*n,*/NumPinos,bosquex,bosquey){
 
       var matrixc = new Matrix4();
 
-      modelMatrix.push(new Pino("P1",positionx,positiony,0,matrixc));
+      myScene.push(new Pino(positionx,positiony,0,matrixc));
 
-      modelMatrix[i].matrix = modelMatrix[i].matrix.translate(modelMatrix[i].x,modelMatrix[i].y,0);
-      modelMatrix[i].matrix = modelMatrix[i].matrix.scale(Sx,Sy,Sz);
-      modelMatrix[i].matrix = modelMatrix[i].matrix.rotate(angRotation,0,0,1);
+      myScene[i].mMatrix = myScene[i].mMatrix.translate(myScene[i].x,myScene[i].y,0);
+      myScene[i].mMatrix = myScene[i].mMatrix.scale(Sx,Sy,Sz);
+      myScene[i].mMatrix = myScene[i].mMatrix.rotate(angRotation,0,0,1);
 
-      mvpMatrix.set(projMatrix).multiply(viewMatrix).multiply(modelMatrix[i].matrix);
+      //mvpMatrix.set(projMatrix).multiply(viewMatrix).multiply(myScene[i].mMatrix);
 
       //Esto de auí abajo sería para pintar la primera vez sin el bucle.
 
@@ -211,16 +228,60 @@ function keydown(ev){
 
 }
 
-function initPinoBuffers(){
+function initFloorBuffers() {
 
-   pinoVarBuffer1.pinoVerticesBuffer = gl.createBuffer();
-   
-   gl.bindBuffer(gl.ARRAY_BUFFER, pinoVarBuffer1.pinoVerticesBuffer);
+   //objfloorvar1.n = objfloorvar1.n+1;
 
-   var pinoVertices = new Float32Array([
+   myBuffers[0].VerticesBuffer = gl.createBuffer();
+   gl.bindBuffer(gl.ARRAY_BUFFER, myBuffers[0].VerticesBuffer);
+
+   var floorVertices = new Float32Array([
 
       //-1.0, -1.0, 0.0,  -1.0,  1.0, 0.0,  1.0,  1.0, 0.0,
       //  1.0, -1.0, 0.0,    // Plano suelo
+
+      -1.0, -1.0, 0.0,  1.0, -1.0, 0.0, -1.0, 1.0, 0.0, //t1 izquierdo
+       1.0, -1.0, 0.0, -1.0,  1.0, 0.0,  1.0, 1.0, 0.0,  //t2 derecho
+
+   ]);
+
+   gl.bufferData(gl.ARRAY_BUFFER, floorVertices, gl.STATIC_DRAW);
+
+
+   
+   myBuffers[0].VerticesTextureCoordBuffer = gl.createBuffer();
+   gl.bindBuffer(gl.ARRAY_BUFFER, myBuffers[0].VerticesTextureCoordBuffer);
+
+
+   var textureCoordinates = new Float32Array([
+      //0.0,  0.0, 50.0, 0.0, 50.0,  50.0, 0.0,  50.0,  // Plano suelo
+
+      0.0,  0.0,     50.0,  0.0,     50.0,  50.0,     0.0,  50.0,  // Front
+      0.0,  0.0,     50.0,  0.0,     50.0,  50.0,     0.0,  50.0  //Botom*/
+   ]);
+
+   gl.bufferData(gl.ARRAY_BUFFER, textureCoordinates, gl.STATIC_DRAW);
+
+   myBuffers[0].VerticesIndicesBuffer = gl.createBuffer();
+   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, myBuffers[0].VerticesIndicesBuffer);
+
+   var floorVerticesIndices = new Uint16Array([
+       //0,  1,  2,      0,  2,  3,
+      0,1,2,  3,4,5 //floor
+   ])
+   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, floorVerticesIndices, gl.STATIC_DRAW);
+
+   }
+
+
+
+function initPinoBuffers(){
+
+   myBuffers[1].VerticesBuffer = gl.createBuffer();
+   
+   gl.bindBuffer(gl.ARRAY_BUFFER, myBuffers[1].VerticesBuffer);
+
+   var pinoVertices = new Float32Array([
 
       0.0, 0.0, 0.5,   0.25, 0.25, 0.0,  -0.25,-0.25, 0.0, //Triangulo 1
       0.0, 0.0, 0.5,  -0.25, 0.25, 0.0,   0.25,-0.25,-0.0  //Triangulo 2
@@ -229,11 +290,8 @@ function initPinoBuffers(){
    ]);
 
    gl.bufferData(gl.ARRAY_BUFFER, pinoVertices, gl.STATIC_DRAW);
-
-
-   
-   pinoVarBuffer1.pinoVerticesTextureCoordBuffer = gl.createBuffer();
-   gl.bindBuffer(gl.ARRAY_BUFFER, pinoVarBuffer1.pinoVerticesTextureCoordBuffer);
+   myBuffers[1].VerticesTextureCoordBuffer = gl.createBuffer();
+   gl.bindBuffer(gl.ARRAY_BUFFER, myBuffers[1].VerticesTextureCoordBuffer);
 
 
    var textureCoordinates = new Float32Array([
@@ -244,36 +302,50 @@ function initPinoBuffers(){
    ]);
 
    gl.bufferData(gl.ARRAY_BUFFER, textureCoordinates, gl.STATIC_DRAW);
-
-   pinoVarBuffer1.pinoVerticesIndicesBuffer = gl.createBuffer();
-   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, pinoVarBuffer1.pinoVerticesIndicesBuffer);
+   myBuffers[1].VerticesIndicesBuffer = gl.createBuffer();
+   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, myBuffers[1].VerticesIndicesBuffer);
 
    var pinoVerticesIndices = new Uint16Array([
-       //0,  1,  2,      0,  2,  3,
-      0,1,2,  3,4,5 //pino
+         0,1,2,  3,4,5 //pino
    ])
    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, pinoVerticesIndices, gl.STATIC_DRAW);
 
    }
 
+   function initFloorTextures() {
+      var y = 0;
+   myBuffers[y].Texture = gl.createTexture();
+   console.log(myBuffers[y].Texture);
 
-function initTextures() {
-   pinoVarBuffer1.pinoTexture = gl.createTexture();
-   console.log(pinoVarBuffer1.pinoTexture);
-
-   gl.bindTexture(gl.TEXTURE_2D, pinoVarBuffer1.pinoTexture);//esto se puede borrar
+   gl.bindTexture(gl.TEXTURE_2D, myBuffers[y].Texture);//esto se puede borrar
    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, //esto se puede borrar
               new Uint8Array([0, 0, 255, 255])); //esto se puede borrar
 
-   var pinoImage = new Image();
-   pinoImage.onload = function() { handleTextureLoaded(pinoImage); }
-   pinoImage.src = "resources/pino.jpg";
+   var image = new Image();
+   image.onload = function() { handleTextureLoaded(y,image); }
+   image.src = "resources/hierba2.jpg";
 }
 
-function handleTextureLoaded(image) {
+
+function initPinoTextures() {
+   var y = 1;
+   myBuffers[y].Texture = gl.createTexture();
+
+   console.log(myBuffers[y].Texture);
+
+   gl.bindTexture(gl.TEXTURE_2D, myBuffers[y].Texture);//esto se puede borrar
+   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, //esto se puede borrar
+              new Uint8Array([0, 0, 255, 255])); //esto se puede borrar
+
+   var image = new Image();
+   image.onload = function() { handleTextureLoaded(y,image); }
+   image.src = "resources/pino.jpg";
+}
+
+function handleTextureLoaded(y,image) {
    console.log("handleTextureLoaded, image = " + image);
 
-   gl.bindTexture(gl.TEXTURE_2D, pinoVarBuffer1.pinoTexture);
+   gl.bindTexture(gl.TEXTURE_2D, myBuffers[y].Texture);
    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
         gl.UNSIGNED_BYTE, image);
    //console.log(objfloorvar1.floorTexture);
@@ -283,58 +355,6 @@ function handleTextureLoaded(image) {
    gl.bindTexture(gl.TEXTURE_2D, null);
 
 }
-
-
-/*function initVertexBuffers(gl) {
-
-var verticesColors = new Float32Array([
-   //t1 Dibujo el primer
-   0.0, 0.0, 0.5,  0.0,  1.0,  0.0,
-   0.25, 0.25, 0.0,  0.0,  1.0,  0.0,
-   -0.25, -0.25, 0.0,  0.0,  1.0,  0.0,
-
-      //t2 Dibujo el segundo
-
-   0.0, 0.0, 0.5,  0.0,  0.5,  0.0,
-   -0.25, 0.25, 0.0,  0.0,  0.5,  0.0,
-   0.25, -0.25, -0.0,  0.0,  0.5,  0.0,
-
-   ]);
-
-   var n =6;
-
-  // Create a buffer object
-  var vertexColorBuffer = gl.createBuffer();
-  if (!vertexColorBuffer) {
-    console.log('Failed to create the buffer object');
-    return -1;
-  }
-
-
-   gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
-   gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW);
-
-   var FSIZE = verticesColors.BYTES_PER_ELEMENT;
-
-   var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-   if(a_Position < 0) {
-      console.log('Failed to get the storage location of a_Position');
-      return -1;
-  }
-   gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, FSIZE * 6, 0);
-   gl.enableVertexAttribArray(a_Position);
-
-   var a_Color = gl.getAttribLocation(gl.program, 'a_Color');
-   if(a_Color < 0) {
-      console.log('Failed to get the storage location of a_Color');
-      return -1;
-  }
-   gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 6, FSIZE * 3);
-   gl.enableVertexAttribArray(a_Color);
-
-   return n;
-}*/
-
 
 function main() {
    var canvas = document.getElementById('webgl');
@@ -356,16 +376,33 @@ function main() {
    var anglez = 0.0;
 
    //variables buffer texturas pino
-   var pinoTexture;
-   var pinoVerticesBuffer
-   var pinoVerticesTextureCoordBuffer;
-   var pinoVerticesIndicesBuffer; 
+   var Texture;
+   var VerticesBuffer
+   var VerticesTextureCoordBuffer;
+   var VerticesIndicesBuffer; 
+
+   //Arrays
+
+   //Por el momento voy a probar con variables glovaless, después las pondré aquí.
+   //var myScene = [];
 
 
    camara1 = new camara(pasos,angle,pasosx,pasosy,speed,moveAngle,alturaOjos,anglez);
 
-   pinoVarBuffer1 = new pinoVarBuffer(pinoTexture,pinoVerticesBuffer,pinoVerticesTextureCoordBuffer,
-               pinoVerticesIndicesBuffer); 
+   myBuffers.push(new floorVarBuffer(Texture,VerticesBuffer,VerticesTextureCoordBuffer,
+               VerticesIndicesBuffer));
+
+   myBuffers.push(new pinoVarBuffer(Texture,VerticesBuffer,VerticesTextureCoordBuffer,
+               VerticesIndicesBuffer));
+
+
+   projMatrix.setPerspective(90, canvas.width/canvas.height, 1, 100);
+   mMatrix = new Matrix4();
+   mMatrix.scale(BOSQUEX,BOSQUEY,1);
+
+   myScene.push(new Floor(mMatrix));
+
+
 
    gl = getWebGLContext(canvas);
    if (!gl) {
@@ -379,13 +416,6 @@ function main() {
       return;
    }
 
-   // Set the vertex coordinates and color
-   //var n = initVertexBuffers(gl);
-   //if (n < 0) {
-     // console.log('Failed to set the vertex information');
-      //return;
-  // }
-
   // Specify the color for clearing <canvas>
 
   //Inicio para las texturas
@@ -395,12 +425,6 @@ function main() {
    gl.enable(gl.DEPTH_TEST);           // Enable depth testing
    gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
 
-   //incio de canvas pero sin texturas
-
-   //gl.clearColor(0, 0, 0, 1);
-   //gl.enable(gl.DEPTH_TEST);
-
-
    // Get the storage location of u_MvpMatrix
    u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
 
@@ -408,11 +432,17 @@ function main() {
       console.log('Failed to get the storage location of u_MvpMatrix');
       return;
    }
-   projMatrix.setPerspective(90, canvas.width/canvas.height, 1, 100);
-   plantarPino(/*n,*/NUMPINOS,BOSQUEX,BOSQUEY);
+   
+   plantarPino(NUMPINOS,BOSQUEX,BOSQUEY);
 
+   
    initPinoBuffers();
-   initTextures();
+   initFloorBuffers();
+  
+   initPinoTextures();
+   initFloorTextures();
+
+
    drawScene();
 
    document.onkeydown = function(ev){
